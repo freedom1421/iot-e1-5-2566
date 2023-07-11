f1d79f85fff46dd1bac892064afdc13a15f589ae
freedom1421 <d50565@ccollege.ac.th>
2023-07-11 12:01:34 +0700

    init2

--- ex01.js
+++ ex01.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3003
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! rueangsak')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log('Krujame')
 })
